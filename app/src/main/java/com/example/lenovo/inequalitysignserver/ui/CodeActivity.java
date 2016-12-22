3c7aa367ab8e3a24d7eb34d4ca10cdda5b616c01
公菲菲 <15230810870@163.com>
2016-12-22 10:45:06 +0800

    取号二维码功能

--- app/src/main/java/com/example/lenovo/inequalitysignserver/ui/CodeActivity.java
+++ app/src/main/java/com/example/lenovo/inequalitysignserver/ui/CodeActivity.java
@@ -1,15 +1,23 @@
 package com.example.lenovo.inequalitysignserver.ui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.lenovo.inequalitysignserver.R;
+import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
 public class CodeActivity extends AppCompatActivity {
 
     private ImageButton mBtnBack;
+    private ImageView mIvCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,20 @@ public class CodeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_code);
         findView();
         setListener();
+        SharedPreferences sharedPreferences = getSharedPreferences("ACCOUNT", Context.MODE_APPEND);
+        String smallimg = sharedPreferences.getString("SIMG", "");
+        Bitmap logo;
+        if (!smallimg.isEmpty()) {
+            byte []b = Base64.decode(smallimg, Base64.DEFAULT);
+            logo = BitmapFactory.decodeByteArray(b, 0, b.length);
+        } else {
+            logo = BitmapFactory.decodeResource(getResources(), R.drawable.logo);
+        }
+
+        SharedPreferences spf = getSharedPreferences("ACCOUNT", MODE_APPEND);
+        String id = spf.getString("ID", "");
+        Bitmap qrCodeBitmap = EncodingUtils.createQRCode(id, 400, 400,logo);
+        mIvCode.setImageBitmap(qrCodeBitmap);
     }
 
     private void setListener() {
@@ -30,5 +52,6 @@ public class CodeActivity extends AppCompatActivity {
 
     private void findView() {
         mBtnBack = (ImageButton) findViewById(R.id.IBtnCodeBack);
+        mIvCode = (ImageView) findViewById(R.id.IvCode);
     }
 }
